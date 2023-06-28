f30e0284a2abb8295f2735b028b0039bc13aaaa6
Gaurav-ps <gauravpshetty96@gmail.com>
2023-06-28 11:20:01 +0530

    SignUp Frontend

---
+++ SignUp/main.js
@@ -0,0 +1,21 @@
+async function onsignup(event){
+    try{
+        event.preventDefault();
+        let obj = {
+            Name: event.target.username.value,
+            Email: event.target.useremail.value,
+            Phone: event.target.phonenumber.value,
+            Password: event.target.password.value,
+            userId: 1
+        }
+        
+        //let postResponse = axios.post('http://3.6.101.169:4000/users/signup',obj)
+        
+        let postResponse = axios.post('http://localhost:4000/users/signup',obj)
+        let val = await postResponse;
+        alert(val.data.message)
+    }
+    catch(err){
+        console.log(err);
+    }
+}
